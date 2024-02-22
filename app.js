ebac8e82548fe81935f0a660ccd3f190ddcad693
Mansuri Villa <113539390+MansuriVilla@users.noreply.github.com>
2024-02-22 15:48:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// footer script for dropdown menu start
+
+jQuery(".nav-folderized h1").click(function () {
+  jQuery(this).parent(".nav").toggleClass("open");
+  jQuery("html, body").animate(
+    { scrollTop: jQuery(this).offset().top - 170 },
+    1500
+  );
+});
+
+// footer script for dropdown menu end
+
+// menu icon
+
+function menu() {
+  var links = document.getElementById("show");
+  links.classList.toggle("right-ul");
+}
+
+function close() {
+  var links = document.getElementById("ss");
+  links.classList.toggle("close");
+}
